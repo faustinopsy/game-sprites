6f435e24321adbbfb87481d712f7c0622f91727e
faustinopsy <rodrigohipnose@gmail.com>
2024-05-10 15:43:55 -0300

    modified:   loading.js

--- loading.js
+++ loading.js
@@ -1,49 +1,52 @@
 const container0 = document.querySelector(`#loading`);
 const container3 = document.querySelector(`.pai-iori`);
 const iori = document.querySelector(`.iori`);
-iori.style.background = `url(iori.png) -270px 0px`;
-let valor=0;
+const BACKGROUND_INICIAL = `url(iori.png) -270px 0px`;
+const BACKGROUND_ATIVO = `url(iori.png) -270px -270px`;
+const POSICAO_SOMADA = 270;
+const POSICAO_MAXIMA = 1200;
+let posicaoY=0;
 let  loop= null;
 let string='';
-let secret ='';
 export function init(){
-  loop = setInterval(mudar, 600);
+  loop = setInterval(iniciarAnimacao, 600);
 }
 
 export function iorix(){
   container0.style.display = 'block';
   container3.style.display = 'block';
-  iori.style.background = `url(iori.png) -270px -270px`;
+  iori.style.background = BACKGROUND_ATIVO;
+}
+function pararAnimacao() {
+  iori.style.background = BACKGROUND_ATIVO;
+  mostrarContainer(false);
 }
 
-function mudar(){
-  valor+=270;
-  iori.style.background = `url(iori.png) -270px -${valor}px`
-    if(valor >= 1200){
+function mostrarContainer(show) {
+  container0.style.display = show ? 'block' : 'none';
+  container3.style.display = show ? 'block' : 'none';
+}
+function iniciarAnimacao(){
+  posicaoY+=POSICAO_SOMADA;
+  iori.style.background = `url(iori.png) -270px -${posicaoY}px`
+    if(posicaoY >= POSICAO_MAXIMA){
     clearInterval(loop);
-    iori.style.background = `url(iori.png) -270px -270px`
-    container0.style.display = 'none'
-    container3.style.display = 'none'
+    pararAnimacao();
     }
 }
 document.addEventListener ('keyup', (event) => {
   
   string +=event.key
   if(string.length >= 9){
-    if(string==='dev-Enter'){
-      secret = string.split('-');
-      
-      iorix()
-      string=''
-      secret=''
-    }else if(string==='zero-Enter'){
-      init()
+    switch(string){
+      case 'dev-Enter':
+        iorix()
+      break;
+      case 'zero-Enter':
+        init()
+      break;
     }
-    else{
-      string=''
-      secret=''
-    }
+    string=''
   }
-  console.log(string)
   
 });
